8f121f0d2ef2127d2c391fa460c4fcc42cac1b7a
n3ke <kolyawin271006@yandex.ru>
2023-01-26 09:56:05 +0300

    os system ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+
 import telebot
 
 bot = telebot.TeleBot("5569374220:AAEoyEM5zkZBNa7QjB-rIiP-Hfs5lideIWo")
@@ -6,9 +7,14 @@ bot = telebot.TeleBot("5569374220:AAEoyEM5zkZBNa7QjB-rIiP-Hfs5lideIWo")
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-# bot.reply_to(message,"You send:" + message.text + "   " + message.from_user.username + "   " + str(message.from_user.id))
+    # bot.reply_to(message,"You send:" + message.text + "   " + message.from_user.username + "   " + str(message.from_user.id))
     bot.send_message(message.chat.id, "Done")
+    file = open("test1.txt")
+    text = file.read()
+    file.close()
     os.system('start test1.txt')
+    bot.send_message(message.chat.id, str(text))
+
 
 # @bot.message_handler(func=lambda message: True)
 # def echo_all(message):
