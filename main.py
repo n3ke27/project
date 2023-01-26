a27b246815099afd6c5a5aac4bebd9f13553f212
n3ke <kolyawin271006@yandex.ru>
2023-01-26 09:40:32 +0300

    os system ready

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import os
 import telebot
 
 bot = telebot.TeleBot("5569374220:AAEoyEM5zkZBNa7QjB-rIiP-Hfs5lideIWo")
 
+
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-	bot.reply_to( message, "You send:" + message.text + "   " + message.from_user.username + "   " + str(message.from_user.id))  
-    
+# bot.reply_to(message,"You send:" + message.text + "   " + message.from_user.username + "   " + str(message.from_user.id))
+    bot.send_message(message.chat.id, "Done")
+    os.system('start test1.txt')
 
-#@bot.message_handler(func=lambda message: True)
-#def echo_all(message):
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
 #	bot.reply_to(message, message.text)
 
 @bot.message_handler(commands=['add'])
 def greatings(message):
-    pass
+    bot.send_message(message.chat.id, "Done")
+    os.system("project/test1.txt")
 
 
 bot.infinity_polling(interval=0, timeout=20)
